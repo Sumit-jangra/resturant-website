8244b2446d2a484c6dc3d2764d14cdb191ccc652
Sumit-jangar <sj867363@gmail.com.in>
2024-09-23 17:46:12 +0530

    ResturentWebsite

---
+++ src/Components/data.js
@@ -0,0 +1,95 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+import Salad from "../images/salad.jpg";
+import Burger from "../images/burger.jpg";
+import Burgar from "../images/burgar.jpg";
+import Capsicum from "../images/capsicum.jpg";
+import Tomatopizza from "../images/tomatopizza.jpg";
+import VegPizza from "../images/vegpizza.jpg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+  {
+    name: "salad",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Salad,
+    price: 200,
+  },
+  {
+    name: "Burger",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Burger,
+    price: 100,
+  },  {
+    name: "Burgar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Burgar,
+    price: 150,
+  },  {
+    name: "Capsicum",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Capsicum,
+    price: 200,
+  },  {
+    name: "Tomatopizza",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Tomatopizza,
+    price: 200,
+  },  {
+    name: "VegPizza",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: VegPizza,
+    price: 200,
+  },
+];
